ee3789415074c72410331f106a951eef155a7a02
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2024-12-04 18:36:50 +0100

    day 2

---
+++ AdventOfCode2024/Day2.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2024;
+
+public class Day2 : ISolvable
+{
+    public string SolvePart1(string[] input) => ParseInput(input).Count(IsSafe).ToString();
+
+    public string SolvePart2(string[] input)
+    {
+        return ParseInput(input)
+               .Count(line => IsSafe(line) || AnySafe(line))
+               .ToString();
+
+        bool AnySafe(int[] line) => Enumerable.Range(0, line.Length)
+                                              .Select(i => line.Take(i).Concat(line.Skip(i + 1)).ToArray())
+                                              .Any(IsSafe);
+    }
+
+    private static bool IsSafe(int[] numLine)
+    {
+        var diffs = numLine.Zip(numLine.Skip(1), (x, y) => x - y).ToList();
+        var isLinear = Enumerable.Range(1, diffs.Count - 1).All(i => diffs[i] * diffs[i - 1] > 0);
+
+        return isLinear && diffs.All(x => Math.Abs(x) <= 3);
+    }
+
+    private static IEnumerable<int[]> ParseInput(string[] input) => input.Select(x => x.Split().Select(int.Parse).ToArray());
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,10 +1,10 @@
-﻿
-using AdventOfCode2023;
+﻿using AdventOfCode2024;
 
 var lines = File.ReadAllLines("input.txt");
 
-var res1 = new Day1().SolvePart1(lines);
+var day = new Day2();
+var res1 = day.SolvePart1(lines);
 Console.WriteLine($"Part 1: {res1}");
 
-var res2 = new Day1().SolvePart2(lines);
+var res2 = day.SolvePart2(lines);
 Console.WriteLine($"Part 2: {res2}");
