9ceb9d3aca10c2e4f50c671d0cd462fff6fc7b1b
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2024-12-05 16:21:49 +0100

    day 3 and 4

---
+++ AdventOfCode2024/Day3.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024;
+
+public class Day3 : ISolvable
+{
+    public string SolvePart1(string[] input)
+    {
+        var pattern = @"mul\((\d+,\d+)\)";
+
+        return input.SelectMany(x => Regex.Matches(x, pattern).Select(y => y.Groups.Values.Last()))
+                    .Select(x => x.Value.Split(',').Select(long.Parse).ToList())
+                    .Select(x => x[0] * x[1])
+                    .Sum()
+                    .ToString();
+    }
+
+    public string SolvePart2(string[] input)
+    {
+        var list = $"do(){string.Join("", input)}".Split("don't()")
+                                                  .SelectMany(x => x.Split("do()")[1..])
+                                                  .ToArray();
+
+        return SolvePart1(list);
+    }
+}
---
+++ AdventOfCode2024/Day4.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2024;
+
+public class Day4 : ISolvable
+{
+    public string SolvePart1(string[] input)
+    {
+        var res = 0;
+        for (var i = 0; i < input.Length; i++)
+        for (var j = 0; j < input[0].Length; j++)
+        {
+            res += FindXmas(input, i, j);
+        }
+
+        return res.ToString();
+    }
+
+    public string SolvePart2(string[] input)
+    {
+        var res = 0;
+        for (var i = 0; i < input.Length; i++)
+        for (var j = 0; j < input[0].Length; j++)
+        {
+            if (FindMasAsX(input, i, j))
+                res++;
+        }
+
+        return res.ToString();
+    }
+
+    private static int FindXmas(string[] input, int i, int j)
+    {
+        var diffs = new[] {(0, 1), (1, 0), (0, -1), (-1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)};
+
+        return diffs.Count(diff => FindWord(input, (i, j), diff, "XMAS"));
+    }
+
+    private static bool FindMasAsX(string[] input, int i, int j)
+    {
+        var foundLeft = false;
+        var foundRight = false;
+        foreach (var parts in new[] {("AS", "AM"), ("AM", "AS")})
+        {
+            if (FindWord(input, (i, j), (-1, -1), parts.Item1) &&
+                FindWord(input, (i, j), (1, 1), parts.Item2))
+                foundLeft = true;
+
+            if (FindWord(input, (i, j), (-1, 1), parts.Item1) &&
+                FindWord(input, (i, j), (1, -1), parts.Item2))
+                foundRight = true;
+        }
+
+        return foundLeft && foundRight;
+    }
+
+    private static bool FindWord(string[] input, (int i, int j) pos, (int i, int j) diff, string word)
+    {
+        var curLet = 0;
+        while (true)
+        {
+            if (curLet == word.Length)
+                return true;
+
+            if (OutOfBorders() || input[pos.i][pos.j] != word[curLet++])
+                return false;
+
+            pos = (i: pos.i + diff.i, j: pos.j + diff.j);
+        }
+
+        bool OutOfBorders() => pos.i < 0 || pos.j < 0 || pos.i >= input.Length || pos.j >= input[0].Length;
+    }
+}
