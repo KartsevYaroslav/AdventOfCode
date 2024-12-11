de15f7bd3ca3aa1f2011e989a8194783e6534886
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2024-12-11 18:28:01 +0100

    day 10

---
+++ AdventOfCode2024/Day10.cs
@@ -0,0 +1,64 @@
+﻿using Shared;
+
+namespace AdventOfCode2024;
+
+public class Day10 : ISolvable
+{
+    public string SolvePart1(string[] input)
+    {
+        var map = ParseInput(input);
+        var starts = CreateDictionary(map, () => new HashSet<(int, int)>());
+
+        Traverse(starts, map, (x, y) =>
+        {
+            x.Add(y);
+            return x;
+        });
+
+        return starts.Values.Sum(x => x.Count).ToString();
+    }
+
+    public string SolvePart2(string[] input)
+    {
+        var map = ParseInput(input);
+        var starts = CreateDictionary(map, () => 0);
+
+        Traverse(starts, map, (x, _) => x + 1);
+
+        return starts.Values.Sum().ToString();
+    }
+
+    private static void Traverse<T>(Dictionary<(int i, int j), T> starts, int[][] map, Func<T, (int, int), T> act)
+    {
+        foreach (var start in starts.Keys)
+        {
+            var stack = new Stack<((int i, int j) curPos, (int, int) start)>();
+            stack.Push((start, start));
+            while (stack.Count != 0)
+            {
+                var (curPos, initStart) = stack.Pop();
+
+                if (map[curPos.i][curPos.j] == 9)
+                {
+                    starts[initStart] = act(starts[initStart], curPos);
+                    continue;
+                }
+
+                foreach (var (i1, j1) in map.GetDirectNeighbours(curPos))
+                {
+                    if (map[i1][j1] - map[curPos.i][curPos.j] != 1)
+                        continue;
+
+                    stack.Push(((i1, j1), initStart));
+                }
+            }
+        }
+    }
+
+    private static Dictionary<(int i, int j), T> CreateDictionary<T>(int[][] map, Func<T> value) =>
+        map.SelectMany((x, i) => x.Select((_, j) => (i, j)))
+           .Where(x => map[x.i][x.j] == 0)
+           .ToDictionary(x => x, _ => value());
+
+    private static int[][] ParseInput(string[] input) => input.Select(x => x.Select(ch => ch - '0').ToArray()).ToArray();
+}
--- Shared/Extensions.cs
+++ Shared/Extensions.cs
@@ -15,4 +15,36 @@ public static class Extensions
             Console.WriteLine(new string(line));
         }
     }
+    
+    public static IEnumerable<(int i1, int j1)> GetDirectNeighbours<T>(this T[][] input, (int i, int j) pos)
+    {
+        if (pos.i < input.Length - 1)
+            yield return (pos.i + 1, pos.j);
+        if (pos.i > 0)
+            yield return (pos.i - 1, pos.j);
+        if (pos.j < input[0].Length - 1)
+            yield return (pos.i, pos.j + 1);
+        if (pos.j > 0)
+            yield return (pos.i, pos.j - 1);
+    }
+    public static IEnumerable<(int i1, int j1)> GetAllNeighbours<T>(this T[][] input, (int i, int j) pos)
+    {
+        if (pos.i < input.Length - 1)
+            yield return (pos.i + 1, pos.j);
+        if (pos.i > 0)
+            yield return (pos.i - 1, pos.j);
+        if (pos.j < input[0].Length - 1)
+            yield return (pos.i, pos.j + 1);
+        if (pos.j > 0)
+            yield return (pos.i, pos.j - 1);
+        if (pos.i < input.Length - 1 && pos.j < input[0].Length - 1)
+            yield return (pos.i + 1, pos.j + 1);
+        if (pos is {i: > 0, j: > 0})
+            yield return (pos.i - 1, pos.j - 1);
+        if (pos.i < input.Length - 1 && pos.j > 0)
+            yield return (pos.i + 1, pos.j - 1);
+        if (pos.i > 0 && pos.j < input[0].Length - 1)
+            yield return (pos.i - 1, pos.j + 1);
+    }
+
 }
