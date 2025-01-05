a57d7e28ce380943b9fd488c35fbd9abd18be802
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2025-01-05 18:23:35 +0100

    day 19

---
+++ AdventOfCode2024/Day19.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2024;
+
+public class Day19 : ISolvable<long>
+{
+    public long SolvePart1(string[] input)
+    {
+        var towels = input.First().Split(',').Select(x => x.Trim()).ToHashSet();
+        var patterns = input.Skip(2).ToList();
+        var cache = new Dictionary<string, long>();
+
+        return patterns.Select(TrySolve)
+                       .Count(x => x.Any(isPossible => isPossible));
+
+        IEnumerable<bool> TrySolve(string pattern)
+            => Enumerable.Range(0, pattern.Length).Select(i => CountSolutions(pattern, 0, i, towels, cache) > 0);
+    }
+
+    public long SolvePart2(string[] input)
+    {
+        var towels = input.First().Split(',').Select(x => x.Trim()).ToHashSet();
+        var patterns = input.Skip(2).ToList();
+        var cache = new Dictionary<string, long>();
+
+        return patterns.Select(PatternSolutions)
+                       .Sum(x => x.Sum());
+
+        IEnumerable<long> PatternSolutions(string pattern)
+            => Enumerable.Range(0, pattern.Length).Select(i => CountSolutions(pattern, 0, i, towels, cache));
+    }
+
+    private static long CountSolutions(string pattern, int start, int end, HashSet<string> towels, Dictionary<string, long> cache)
+    {
+        var containsTowel = towels.Contains(pattern.Substring(start, end - start + 1));
+        if (end == pattern.Length - 1 && containsTowel)
+            return 1;
+
+        if (!containsTowel)
+            return 0;
+
+        var newStart = end + 1;
+        var remainPattern = pattern.Substring(newStart, pattern.Length - newStart);
+        if (cache.TryGetValue(remainPattern, out var solutions))
+            return solutions;
+
+        var solutionsCount = Enumerable.Range(newStart, pattern.Length - newStart).Sum(i => CountSolutions(pattern, newStart, i, towels, cache));
+        cache.Add(remainPattern, solutionsCount);
+
+        return solutionsCount;
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -2,7 +2,7 @@
 
 var lines = File.ReadAllLines("input.txt");
 
-ISolvable<string> day = new Day18();
+ISolvable<long> day = new Day19();
 var res1 = day.SolvePart1(lines);
 Console.WriteLine($"Part 1: {res1}");
 
