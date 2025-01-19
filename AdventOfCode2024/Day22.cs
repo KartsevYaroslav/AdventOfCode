b6d7aec8544c8790ae22080eebd065be71ddbc87
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2025-01-19 21:51:50 +0100

    refac day 22

--- AdventOfCode2024/Day22.cs
+++ AdventOfCode2024/Day22.cs
@@ -2,21 +2,23 @@
 
 public class Day22 : ISolvable<long>
 {
+    private readonly List<Func<long, long>> _steps =
+    [
+        x => ((x * 64) ^ x) % 16777216,
+        x => ((x / 32) ^ x) % 16777216,
+        x => ((x * 2048) ^ x) % 16777216
+    ];
+
+    private const int GenerationsCount = 2000;
+
     public long SolvePart1(string[] input)
     {
-        const int generationsCount = 2000;
-        var steps = new List<Func<long, long>>
-        {
-            x => ((x * 64) ^ x) % 16777216,
-            x => ((x / 32) ^ x) % 16777216,
-            x => ((x * 2048) ^ x) % 16777216
-        };
         var res = 0L;
         foreach (var num in input.Select(long.Parse))
         {
             var newNum = num;
-            for (var j = 0; j < generationsCount; j++)
-                newNum = steps.Aggregate(newNum, (current, step) => step(current));
+            for (var j = 0; j < GenerationsCount; j++)
+                newNum = _steps.Aggregate(newNum, (current, step) => step(current));
 
             res += newNum;
         }
@@ -26,70 +28,43 @@ public class Day22 : ISolvable<long>
 
     public long SolvePart2(string[] input)
     {
-        const int generationsCount = 1999;
-        var steps = new List<Func<long, long>>
-        {
-            x => ((x * 64) ^ x) % 16777216,
-            x => ((x / 32) ^ x) % 16777216,
-            x => ((x * 2048) ^ x) % 16777216
-        };
-        var diffsList = new List<List<long>>();
-
-        var sequences = new HashSet<(long, long, long, long)>();
-        var nums = input.Select(long.Parse).ToList();
-        foreach (var num in nums)
+        var memo = new Dictionary<(long, long, long, long), long>();
+        foreach (var num in input.Select(long.Parse))
         {
             var newNum = num;
             var diffs = new List<long>();
             var last = num % 10;
-            for (var j = 0; j < generationsCount; j++)
+            for (var j = 0; j < GenerationsCount - 1; j++)
             {
-                newNum = steps.Aggregate(newNum, (current, step) => step(current));
-
+                newNum = _steps.Aggregate(newNum, (current, step) => step(current));
                 var reminder = newNum % 10;
                 diffs.Add(reminder - last);
                 last = reminder;
             }
 
-            for (var i = 0; i < diffs.Count - 4; i++)
-            {
-                var sequence = (diffs[i], diffs[i + 1], diffs[i + 2], diffs[i + 3]);
-                sequences.Add(sequence);
-            }
-
-            diffsList.Add(diffs);
+            Index(num, diffs, memo);
         }
 
-        var maxSum = long.MinValue;
-        var sequencesList = sequences.ToList();
-        
-        var memo = new Dictionary<(long, (long, long, long, long)), long>();
-        for (var i = 0; i < nums.Count; i++)
-        {
-            var diffs = diffsList[i];
-            Index(nums[i], diffs, memo);
-        }
-        
-        for (var j = 0; j < sequences.Count; j++)
-        {
-            var sum = nums.Sum(num => memo.GetValueOrDefault((num, sequencesList[j]), 0));
-            maxSum = Math.Max(maxSum, sum);
-        }
-
-        return maxSum;
+        return memo.Max(x => x.Value);
     }
-    private static void Index(long num, List<long> diffs, Dictionary<(long, (long, long, long, long)), long> dict)
+
+    private static void Index(long num, List<long> diffs, Dictionary<(long, long, long, long), long> dict)
     {
         var truncated = num % 10;
+        var visited = new HashSet<(long, long, long, long)>();
         for (var i = 0; i < diffs.Count - 4; i++)
         {
             var i1 = i;
             var (d1, d2, d3, d4) = (diffs[i1++], diffs[i1++], diffs[i1++], diffs[i1]);
             var newNum = d1 + d2 + d3 + d4 + truncated;
-            if (!dict.ContainsKey((num, (d1, d2, d3, d4))))
-                dict[(num, (d1, d2, d3, d4))] = newNum;
-
             truncated += diffs[i];
+
+            if (!visited.Add((d1, d2, d3, d4)))
+                continue;
+
+            if (!dict.ContainsKey((d1, d2, d3, d4)))
+                dict[(d1, d2, d3, d4)] = 0;
+            dict[(d1, d2, d3, d4)] += newNum;
         }
     }
 }
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,10 +1,15 @@
-﻿using AdventOfCode2024;
+﻿using System.Diagnostics;using AdventOfCode2024;
 
 var lines = File.ReadAllLines("input.txt");
 
+var stopwatch = new Stopwatch();
 ISolvable<long> day = new Day22();
+stopwatch.Start();
 var res1 = day.SolvePart1(lines);
-Console.WriteLine($"Part 1: {res1}");
+stopwatch.Stop();
+Console.WriteLine($"Part 1: {res1}, Elapsed: {stopwatch.ElapsedMilliseconds} ms");
 
+stopwatch.Restart();
 var res2 = day.SolvePart2(lines);
-Console.WriteLine($"Part 2: {res2}");
+stopwatch.Stop();
+Console.WriteLine($"Part 2: {res2}, Elapsed: {stopwatch.ElapsedMilliseconds} ms");
