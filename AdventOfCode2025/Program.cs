a571637607e86ecdbf8bb113fd96f20244281f0f
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2025-12-03 16:17:03 +0100

    add project 25

---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+var lines = File.ReadAllLines("input.txt");
+
+var stopwatch = new Stopwatch();
+ISolvable<long> day = null;
+stopwatch.Start();
+var res1 = day.SolvePart1(lines);
+stopwatch.Stop();
+Console.WriteLine($"Part 1: '{res1}', Elapsed: {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Restart();
+var res2 = day.SolvePart2(lines);
+stopwatch.Stop();
+Console.WriteLine($"Part 2: '{res2}', Elapsed: {stopwatch.ElapsedMilliseconds} ms");
