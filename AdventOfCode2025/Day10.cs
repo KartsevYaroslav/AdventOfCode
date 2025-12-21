a83ef0198170516692d0a059fecedf9abd23972c
iaroslav.kartsev <iaroslav.kartsev@veeam.com>
2025-12-22 00:19:53 +0100

    lil refac

--- AdventOfCode2025/Day10.cs
+++ AdventOfCode2025/Day10.cs
@@ -15,41 +15,40 @@ public class Day10 : ISolvable<long>
         {
             var list = line.Split().ToList();
             var goalLights = list[0][1..^1];
-            var buttons = list[1..^1]
-                .Select(x => x[1..^1].Split(',').Select(int.Parse).ToArray())
-                .ToList();
+            var buttons = ParseButtons(list);
             res += GetCombinations(goalLights, buttons).First().Count;
         }
 
         return res;
     }
 
-    private static IEnumerable<List<int>> GetCombinations(string goalLights, List<int[]> buttons)
-    {
-        var longs = buttons.Select(x => x.Select(y => (long)Math.Pow(2, y)).Sum()).ToList();
-        var goalNum = Convert.ToInt64(string.Join("", goalLights.Select(x => x == '#' ? "1" : "0").Reverse()), 2);
-        return GetMinCombinations(longs, goalNum);
-    }
-
     public long SolvePart2(string[] input)
     {
         var res = 0L;
-        var c = 1;
         foreach (var line in input)
         {
             var list = line.Split().ToList();
             var goalNums = list.Last()[1..^1].Split(',').Select(int.Parse).ToArray();
-            var buttons = list[1..^1]
-                .Select(x => x[1..^1].Split(',').Select(int.Parse).ToArray())
-                .ToList();
+            var buttons = ParseButtons(list);
 
             res += SolveFinal(goalNums, buttons, new Dictionary<string, long>());
-
-            Console.WriteLine($"Line {c}, result: {res}");
-            c++;
         }
 
         return res;
+    }
+
+    private static List<int[]> ParseButtons(List<string> list)
+    {
+        return list[1..^1]
+            .Select(x => x[1..^1].Split(',').Select(int.Parse).ToArray())
+            .ToList();
+    }
+
+    private static IEnumerable<List<int>> GetCombinations(string goalLights, List<int[]> buttons)
+    {
+        var longs = buttons.Select(x => x.Select(y => (long)Math.Pow(2, y)).Sum()).ToList();
+        var goalNum = Convert.ToInt64(string.Join("", goalLights.Select(x => x == '#' ? "1" : "0").Reverse()), 2);
+        return GetCombinations(longs, goalNum);
     }
 
     private static long SolveFinal(int[] goalNums, List<int[]> buttons, Dictionary<string, long> dictionary)
@@ -59,11 +58,11 @@ public class Day10 : ISolvable<long>
 
         if (goalNums.Any(x => x < 0))
             return int.MaxValue;
-        
+
         var key = string.Join("", goalNums);
         if (dictionary.TryGetValue(key, out var res))
             return res;
-        
+
         dictionary[key] = int.MaxValue;
         if (goalNums.All(y => y % 2 == 0))
             dictionary[key] = Math.Min(DivideTwice(goalNums), dictionary[key]);
@@ -75,11 +74,11 @@ public class Day10 : ISolvable<long>
             foreach (var i in indexes)
                 goalNums[i]--;
 
-            var solveFinal = DivideTwice(goalNums);
+            var result = DivideTwice(goalNums);
             foreach (var i in indexes)
                 goalNums[i]++;
 
-            dictionary[key] =  Math.Min(solveFinal + combination.Count, dictionary[key]);
+            dictionary[key] = Math.Min(result + combination.Count, dictionary[key]);
         }
 
         return dictionary[key];
@@ -98,7 +97,7 @@ public class Day10 : ISolvable<long>
         }
     }
 
-    private static IEnumerable<List<int>> GetMinCombinations(List<long> buttons, long goalNum)
+    private static IEnumerable<List<int>> GetCombinations(List<long> buttons, long goalNum)
     {
         var queue = new Queue<(int buttonIndex, long curNum, List<int> buttons)>();
         for (var i = 0; i < buttons.Count; i++)
